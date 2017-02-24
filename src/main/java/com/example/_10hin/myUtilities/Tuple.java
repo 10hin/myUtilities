a5539101bc93e267547c72d107bbbf942b08218a
10hin <10hin@outlook.jp>
2017-02-25 02:47:58 +0900

    タプルを追加

---
+++ src/main/java/com/example/_10hin/myUtilities/Tuple.java
@@ -0,0 +1,23 @@
+package com.example._10hin.myUtilities;
+
+/**
+ * 不変な2つ組<br>
+ *
+ * @author kei
+ *
+ */
+public interface Tuple<CAR, CDR> {
+
+	/**
+	 * 第1要素
+	 * @return この組の第1要素
+	 */
+	CAR getCar();
+
+	/**
+	 * 第2要素
+	 * @return この組の第2要素
+	 */
+	CDR getCdr();
+
+}
---
+++ src/main/java/com/example/_10hin/myUtilities/TupleImpl.java
@@ -0,0 +1,22 @@
+package com.example._10hin.myUtilities;
+
+public class TupleImpl<CAR, CDR> implements Tuple<CAR, CDR> {
+
+	private final CAR car;
+	private final CDR cdr;
+	protected TupleImpl(CAR car, CDR cdr) {
+		this.car = car;
+		this.cdr = cdr;
+	}
+	public CAR getCar() {
+		return this.car;
+	}
+	public CDR getCdr() {
+		return this.cdr;
+	}
+
+	public static <CAR, CDR> Tuple<CAR, CDR> of(CAR car, CDR cdr) {
+		return new TupleImpl<>(car, cdr);
+	}
+
+}
